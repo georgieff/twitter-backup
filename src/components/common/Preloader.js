8d811ebf8a21c0b3280abd7c2615b8e1cd1214e0
georgieff <lubo@georgieff.it>
2018-04-24 02:17:02 +0300

    linting

--- src/components/common/Preloader.js
+++ src/components/common/Preloader.js
@@ -1,11 +1,17 @@
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Pop} from 'react-preloading-component';
 
 const Preloader = ({color, size}) => {
     return (
         <Pop color={color || '#9b4dca'} size={parseInt(size) || 8} />
     );
+};
+
+Preloader.propTypes = {
+    color: PropTypes.string,
+    size: PropTypes.int
 };
 
 export default Preloader;
